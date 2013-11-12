da4cc8370aea15d9864c1839d871766bddcc7d61
Carlos Gustavo Ruiz <carlosgruiz@arah.at>
2013-11-12 17:14:15 -0430

    added Geoportal.Bookmarks.Fill function require function to call Geoportal.conf.js file favicon.ico added to img/ folder updates in the index.html file

--- js/Geoportal.conf.js
+++ js/Geoportal.conf.js
@@ -2,5 +2,51 @@
  * License goes here
  */
 
-var Geoportal = Geoportal || {};
-Geoportal.Title = "Geoportal 2.0";
+Geoportal.Page.Title = "Geoportal 2.0";
+
+
+/* 
+ * Geoportal Bookmarks
+ *
+ * Usage: to add a bookmark you may add a JSON object with keys: name, lon, lat and zoom.
+ *        i.e. { "name" : "Caracas", "lon" : -66.88096, "lat": 10.43379, "zoom" : 11 }
+ *
+ */
+
+Geoportal.Bookmarks.List = [
+   {
+       "name" : "My Bookmark",
+       "lon"  : -66,
+       "lat"  : 7.7,
+       "zoom" : 6
+   }
+];
+
+
+/*
+ * Geoportal Map
+ *
+ * Usage: ...
+ *
+ */
+
+Geoportal.Map.Layers = [
+   {
+       "name" : "New WMS Layer",
+       "source" : "WMS",
+       "url" : "",
+       "layers" : "",
+       "projection" : "",
+       "visibility" : false
+   },
+   {
+       "name" : "New KML Layer",
+       "source" : "KML",
+       "url" : "",
+       "visibility" : false
+   },
+   {
+       "name" : "New OSM Layer",
+       "source" : "OSM"
+   }
+]
--- js/Geoportal.js
+++ js/Geoportal.js
@@ -3,21 +3,40 @@
  */
 
 
+
+
 /**
- * Geoportal Object Initialization
+ * Geoportal Objects Initialization
  */
-var Geoportal = Geoportal || {}; 
+var Geoportal = {};
+Geoportal.Page = {};
+Geoportal.Map = {};
+Geoportal.Controls = {};
+Geoportal.Bookmarks = {};
     
 /**
  * Constant: VERSION_NUMBER
  */
 Geoportal.VERSION_NUMBER="2.0a";
 
+
+
+
+
+
 /**
  * Function: Geoportal.Init
  * Starts Geoportal in the index page
  */
 Geoportal.Init = function () {
+
+    // Set Page Data
+    Geoportal.Page.Title = Geoportal.Page.Title || "Geoportal - " + Geoportal.VERSION_NUMBER;
+    document.title = Geoportal.Page.Title;
+
+    // Fill Menus
+    Geoportal.Bookmarks.Fill();
+    // Display Map
     var map = new OpenLayers.Map({
           div: "map",
           theme: null,
@@ -41,3 +60,68 @@ Geoportal.Init = function () {
       });
 };
 
+
+/*
+ * Geoportal.Map Functions
+ */
+Geoportal.Map.Layers = [];
+Geoportal.Map.ZoomToMaxExtent = function() {
+    alert("TODO");
+};
+Geoportal.Map.ZoomToBookmark = function(lon,lat,zoom) {
+    alert("TODO");
+};
+
+Geoportal.Map.Fill = function() {
+    alert("TODO");
+};
+
+/*
+ * Geoportal.Bookmarks Functions
+ */
+Geoportal.Bookmarks.List = [];
+
+Geoportal.Bookmarks.Fill = function() {
+    gb = document.getElementById("geoportal-bookmarks");
+    for (bm in Geoportal.Bookmarks.List){
+        var li = document.createElement("li");
+	li.innerHTML = "<a href=\"#\" onclick=\"Geoportal.Map.ZoomToBookmark(" +
+		       Geoportal.Bookmarks.List[bm].lon + "," +
+		       Geoportal.Bookmarks.List[bm].lat + "," +
+		       Geoportal.Bookmarks.List[bm].zoom + ")\">" +
+		       Geoportal.Bookmarks.List[bm].name + "</a>";
+	gb.appendChild(li);
+    }
+
+    var di = document.createElement("li");
+    di.setAttribute("class","divider");
+    gb.appendChild(di);
+    
+    var last_li = document.createElement("li");
+    last_li.innerHTML = "<a href=\"#\" onclick=\"Geoportal.Map.ZoomToMaxExtent()\">Zoom to Max Extent</a>";
+    gb.appendChild(last_li);
+};
+
+
+
+
+
+/**
+ *  Load Geoportal.conf.js
+ */
+	
+function require(script) {
+    $.ajax({
+        url: script,
+	dataType: "script",
+	async: false,           // <-- This is the key
+	success: function () {
+	    // all good...
+	},
+	error: function () {
+	throw new Error("Could not load script " + script);
+	}
+    });
+}
+
+require("js/Geoportal.conf.js");
